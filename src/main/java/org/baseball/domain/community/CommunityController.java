ea9268458b3d129439e5e1536ad7610828132521
Joo Won <110383412+chqkq@users.noreply.github.com>
2025-06-19 09:49:12 +0900

    feat: css folder

--- src/main/java/org/baseball/domain/community/CommunityController.java
+++ src/main/java/org/baseball/domain/community/CommunityController.java
@@ -1,4 +1,14 @@
 package org.baseball.domain.community;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CommunityController {
+    @GetMapping("/community.do")
+    public String home(Model model) {
+        System.out.println("sout");
+        return "community/community";
+    }
 }
---
+++ src/main/java/org/baseball/domain/community/CommunityService.java
@@ -0,0 +1,7 @@
+package org.baseball.domain.community;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommunityService {
+}
