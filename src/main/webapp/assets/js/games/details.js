ce6b61d31ad4810a55ebf2bfebca6ccf4918ba56
sechang <1031choisechang@gmail.com>
2025-07-08 12:12:35 +0900

    Fix: 경기 경기 결과 없는 월 메세지 추가

--- src/main/webapp/assets/js/games/details.js
+++ src/main/webapp/assets/js/games/details.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
             data: { year, month },
             success: function (data) {
                 if (!data || !data.list || data.list.length === 0) {
-                    alert(`${month}월에는 경기 결과가 존재하지 않습니다..`);
-                    $('#detail-box').empty();
+                    alert(`${month}월에는 경기 결과가 존재하지 않습니다.`);
+                    $('#detail-box').html(`<div id="no-data-msg" class="no-data-msg">해당 달은 경기 결과가 없습니다.</div>`);
                     $('#pagination').empty();
                     return;
                 }
