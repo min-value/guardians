cbd0bf9d9e9e28cbb06be4944b07c8313dcc7c88
subin930 <bread0930@g.skku.edu>
2025-07-09 15:48:45 +0900

    Conflict: QueueService 충돌 해결

--- src/main/java/org/baseball/domain/redis/QueueService.java
+++ src/main/java/org/baseball/domain/redis/QueueService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
