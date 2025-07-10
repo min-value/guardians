9bbf82c27357837be761127dc1a7f9f8793dc4f1
Joo Won <110383412+chqkq@users.noreply.github.com>
2025-07-10 17:00:57 +0900

    Fix: scheduler 시간 변경

--- src/main/java/org/baseball/domain/games/RankCrawler.java
+++ src/main/java/org/baseball/domain/games/RankCrawler.java
@@ -15,7 +15,7 @@ public class RankCrawler {
 
     private final GamesMapper gamesMapper;
 
-    @Scheduled(cron = "0 20 13 * * *")
+    @Scheduled(cron = "0 0 2 * * *")
     public void crawlAndSave() {
         try {
             // 랭킹 페이지 URL
